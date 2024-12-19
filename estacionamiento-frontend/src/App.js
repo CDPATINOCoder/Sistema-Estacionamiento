533a115aa623ba76f07b0acf6488097eb40bff33
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2024-12-19 17:55:02 -0300

    componente App.js modificado, llama al controlador /test para comprobar funcionamiento del proyecto

--- estacionamiento-frontend/src/App.js
+++ estacionamiento-frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery";
 
 function App() {
-  return (
-      <div>
-        <h1>Sistema de Estacionamientos</h1>
-      </div>
-  );
+    useEffect(() => {
+        $.ajax({
+            url: "http://localhost:8080/test",
+            method: "GET",
+            success: (data) => {
+                console.log("Respuesta del backend:", data);
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+                console.error("Error al conectar con el backend:", textStatus);
+            },
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Sistema de Estacionamientos</h1>
+        </div>
+    );
 }
 
 export default App;
