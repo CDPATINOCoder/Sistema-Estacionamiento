f13982f1c361d691417a2e64c75ef30ac1099783
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2024-12-19 13:07:26 -0300

    creacion del proyecto

---
+++ src/main/java/com/una/sistema_estacionamiento/SistemaDeEstacionamientosApplication.java
@@ -0,0 +1,13 @@
+package com.una.sistema_estacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEstacionamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEstacionamientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/una/sistema_estacionamiento/SistemaDeEstacionamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.una.sistema_estacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEstacionamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
